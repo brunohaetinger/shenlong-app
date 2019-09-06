cf93ffdafbbd4e0f578ea9f3ef5c6ee0dfbc55d8
Bruno Haetinger <brunohaetinger@gmail.com>
2019-09-05 21:45:09 -0300

    Remove unused Dragon Detail code

--- src/components/DragonDetail/DragonDetail.js
+++ src/components/DragonDetail/DragonDetail.js
@@ -1,24 +1,7 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core/';
 
-const useStyles = makeStyles(theme => ({
-    modal: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    paper: {
-        backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000',
-        boxShadow: theme.shadows[5],
-        padding: theme.spacing(2, 4, 3),
-    },
-}));
-
 const DragonDetail = ({ isOpen, closeModal, dragon }) => {
-    const classes = useStyles();
-
     const handleClose = () => {
         closeModal();
     };
@@ -36,9 +19,6 @@ const DragonDetail = ({ isOpen, closeModal, dragon }) => {
                     </DialogContentText>
                 </DialogContent>
                 <DialogActions>
-                    {/* <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button> */}
                     <Button onClick={handleClose} color="primary">
                         Close
                     </Button>
