94947fa2ce50e645a129904e0239fecb1f26f3ac
Bruno Haetinger <brunohaetinger@gmail.com>
2019-09-06 23:14:50 -0300

    Set up app theme

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,16 +3,27 @@ import './App.scss';
 import AppRouter from '../AppRouter/AppRouter';
 import { SnackbarProvider } from 'notistack';
 import 'typeface-roboto';
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { lightBlue } from '@material-ui/core/colors/';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: lightBlue
+  },
+});
 
 const App = () => {
   return (
-    <SnackbarProvider maxSnack={3}>
-      <div className="App">
-        <header className="App-header">
-          <AppRouter></AppRouter>
-        </header>
-      </div>
-    </SnackbarProvider>
+    <ThemeProvider theme={theme}>      
+      <SnackbarProvider maxSnack={3}>
+        <div className="App">
+          <header className="App-header">
+            <AppRouter></AppRouter>
+          </header>
+        </div>
+      </SnackbarProvider>
+    </ThemeProvider>
   );
 }
 
